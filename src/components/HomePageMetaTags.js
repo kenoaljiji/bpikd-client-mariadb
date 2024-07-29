8c4c0265e10178e1dacb88011765439d8725acde
kenoaljiji <keno.a89@gmail.com>
2024-07-29 12:44:53 +0200

    change helmet

--- src/components/HomePageMetaTags.js
+++ src/components/HomePageMetaTags.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 export const HomepageMetaTags = ({ title, description, imageUrl, url }) => {
   return (
-    <Helmet>
-      <title>{title}</title>
-      <meta name='description' content={description} />
+    <HelmetProvider>
+      <Helmet>
+        <title>{title}</title>
+        <meta name='description' content={description} />
+        <meta property='image' content='/assets/images/logo-white.png' />
 
-      {/* Open Graph Meta Tags */}
-      <meta property='og:title' content={title} />
-      <meta property='og:site_name' content='Bpikd' />
-      <meta property='og:description' content={description} />
-      <meta property='og:image' content={imageUrl} />
-      <meta property='og:url' content={url} />
-      <meta property='og:type' content='website' />
+        {/* Open Graph Meta Tags */}
+        <meta property='og:title' content={title} />
+        <meta property='og:site_name' content='Bpikd' />
+        <meta property='og:description' content={description} />
+        <meta property='og:image' content={url} />
+        <meta property='og:url' content={url} />
+        <meta property='og:type' content='website' />
 
-      {/* Twitter Card Meta Tags */}
-      <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={description} />
-      <meta name='twitter:image' content={imageUrl} />
-    </Helmet>
+        {/* Twitter Card Meta Tags */}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content={title} />
+        <meta name='twitter:description' content={description} />
+        <meta name='twitter:image' content={imageUrl} />
+      </Helmet>
+    </HelmetProvider>
   );
 };
